1233c9e7c3c2368a467f187621d3bcfc68acd0b7
Jean-Louis GUENEGO <jlguenego@gmail.com>
2024-06-18 17:32:34 +0200

    dev: added typescript

--- back/src/server.ts
+++ back/src/server.ts
@@ -9,7 +9,8 @@ const port = 3000;
 const publicDir = ".";
 
 app.use((req, res, next) => {
-  console.log("req: ", req.method, req.ur1);
+  console.log("this: ", this);
+  console.log("req: ", req.method, req.url);
   next();
 });
 
