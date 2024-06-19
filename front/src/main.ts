16ada4904fb0c78ddf2d249b0021875050de459f
Jean-Louis GUENEGO <jlguenego@gmail.com>
2024-06-19 11:50:17 +0200

    dev: added circle with ts

--- front/src/main.ts
+++ front/src/main.ts
@@ -1,24 +1,21 @@
-import './style.css'
-import typescriptLogo from './typescript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.ts'
+import "./style.css";
 
-document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://www.typescriptlang.org/" target="_blank">
-      <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
-    </a>
-    <h1>Vite + TypeScript</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite and TypeScript logos to learn more
-    </p>
-  </div>
-`
+const svgns = "http://www.w3.org/2000/svg";
+const container = document.querySelector("g.samples");
+if (container === null) {
+  throw new Error("oups");
+}
 
-setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
+const samples = 10;
+const r = 1;
+
+for (let i = 0; i < samples; i++) {
+  const cx = 45;
+  const cy = 34;
+
+  const circle = document.createElementNS(svgns, "circle");
+  circle.setAttributeNS(null, "cx", cx + "");
+  circle.setAttributeNS(null, "cy", cy.toString());
+  circle.setAttributeNS(null, "r", String(r));
+  container.appendChild(circle);
+}
